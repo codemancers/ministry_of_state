3842d29a6b7832fd6fa1af79b6d80e1e58b4d291
Hemant Kumar <gethemant@gmail.com>
2012-03-20 12:53:49 +0530

    Fix deprecation warning coming out of ActiveSupport::Concern

--- lib/ministry_of_state/ministry_of_state.rb
+++ lib/ministry_of_state/ministry_of_state.rb
@@ -90,96 +90,93 @@ module MinistryOfState
     end
   end
 
-  module InstanceMethods
-
-    def set_initial_states
-      states.each do |name, state|
-        next unless state.initial?
-
-        begin
-          enter = state.opts[:enter]
-          invoke_callback(enter) if enter
-          true
-        rescue StandardError => e
-          errors.add(:base, e.to_s)
-          false
-        end
-        default_state = read_attribute( state_machine_column(state.column) )
-        write_attribute(state_machine_column(state.column), state.name) unless default_state
-      end
-    end
-
-    def run_initial_state_actions
-      states.each do |name, state|
-        next unless state.initial?
-
-        begin
-          after = state.opts[:after]
-          invoke_callback(enter) if after
-          true
-        rescue StandardError => e
-          errors.add(:base, e.to_s)
-          false
-        end
-      end
-    end
-
-    def current_state(column)
-      send("#{column}_was").to_sym
-    end
-
-    def check_state(state)
-      column = states[state].column
-      send("#{column}_was") == state.to_s
-    end
-
-    def fire(event)
-      options   = events[event].opts
-      to_state  = options[:to]
-      column    = events[event].column
-
-      raise TransitionNotAllowed.new("Invalid 'to' state '#{to_state}'") unless states[to_state]
-      check_guard = options[:guard] ? invoke_callback(options[:guard]) : true
-      return unless check_guard
-      enter = states[to_state].opts[:enter]
-      after = states[to_state].opts[:after]
-      exit  = states[to_state].opts[:exit]
-      invoke_callback(enter) if enter
+  def set_initial_states
+    states.each do |name, state|
+      next unless state.initial?
 
       begin
-        transaction do
-          self.lock!
-          t_current_state = send( state_machine_column(column) ).try(:to_sym)
-          check_transitions?(t_current_state, options)
-          write_attribute( state_machine_column(column), to_state.to_s )
-          save
-        end
+        enter = state.opts[:enter]
+        invoke_callback(enter) if enter
+        true
+      rescue StandardError => e
+        errors.add(:base, e.to_s)
+        false
+      end
+      default_state = read_attribute( state_machine_column(state.column) )
+      write_attribute(state_machine_column(state.column), state.name) unless default_state
+    end
+  end
 
-        invoke_callback(exit) if exit
-        invoke_callback(after) if after
-        self.errors.empty?
+  def run_initial_state_actions
+    states.each do |name, state|
+      next unless state.initial?
+
+      begin
+        after = state.opts[:after]
+        invoke_callback(enter) if after
+        true
       rescue StandardError => e
         errors.add(:base, e.to_s)
         false
       end
     end
+  end
 
-    def state_machine_column(column)
-      if(self.class.column_names.include?(column))
-        column
-      else
-        raise InvalidStateColumn.new("State column '#{column}' does not exist in table '#{self.class.table_name}'")
+  def current_state(column)
+    send("#{column}_was").to_sym
+  end
+
+  def check_state(state)
+    column = states[state].column
+    send("#{column}_was") == state.to_s
+  end
+
+  def fire(event)
+    options   = events[event].opts
+    to_state  = options[:to]
+    column    = events[event].column
+
+    raise TransitionNotAllowed.new("Invalid 'to' state '#{to_state}'") unless states[to_state]
+    check_guard = options[:guard] ? invoke_callback(options[:guard]) : true
+    return unless check_guard
+    enter = states[to_state].opts[:enter]
+    after = states[to_state].opts[:after]
+    exit  = states[to_state].opts[:exit]
+    invoke_callback(enter) if enter
+
+    begin
+      transaction do
+        self.lock!
+        t_current_state = send( state_machine_column(column) ).try(:to_sym)
+        check_transitions?(t_current_state, options)
+        write_attribute( state_machine_column(column), to_state.to_s )
+        save
       end
-    end
 
-    def check_transitions?(t_current_state, opts)
-      unless opts[:from].include?(t_current_state)
-        raise InvalidState.new("Invalid from state '#{t_current_state}' for target state '#{opts[:to]}'")
-      end
+      invoke_callback(exit) if exit
+      invoke_callback(after) if after
+      self.errors.empty?
+    rescue StandardError => e
+      errors.add(:base, e.to_s)
+      false
     end
+  end
 
-    def invoke_callback(callback)
-      callback.is_a?(Proc) ? callback.call(self) : send(callback)
+  def state_machine_column(column)
+    if(self.class.column_names.include?(column))
+      column
+    else
+      raise InvalidStateColumn.new("State column '#{column}' does not exist in table '#{self.class.table_name}'")
     end
+  end
+
+  def check_transitions?(t_current_state, opts)
+    unless opts[:from].include?(t_current_state)
+      raise InvalidState.new("Invalid from state '#{t_current_state}' for target state '#{opts[:to]}'")
+    end
+  end
+
+  def invoke_callback(callback)
+    callback.is_a?(Proc) ? callback.call(self) : send(callback)
   end
 end
